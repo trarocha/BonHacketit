cc52ea9f686443e89859891789016374e6381c2c
Tracy Rocha <tracyarocha@gmail.com>
2020-09-11 18:46:49 -0700

    Updated column names. Implemented sort by feature.

--- main.py
+++ main.py
@@ -4,34 +4,35 @@
 import pandas as pd
 
 # Choose restaurant
-res = input("\nWelcome!\n\nSelect one of the following restaurants:\n\tPanda Express\n\tTaco Bell\n\n")
+res = input("\nWelcome!\n\nSelect one of the following restaurants:\n\tPanda Express\n\tTaco Bell\n")
 resValid = False
 # print(res)
 
 # Open the restuarant file, if available
 while (resValid == False):
     if (res == "Panda Express"):
-        names = ['Food', 'Type', 'ServingSize', 'Calories', 'CaloriesFromFat', 'TotalFat', 'SaturatedFat', 
-            'TransFat', 'Cholesterol', 'Sodium', 'TotalCarb', 'DietaryFiber', 'Sugars', 'Protein']
+        names = ['Food', 'Type', 'Serving Size', 'Calories', 'Calories From Fat', 'Total Fat', 'Saturated Fat', 
+            'Trans Fat', 'Cholesterol', 'Sodium', 'Total Carb', 'Dietary Fiber', 'Sugars', 'Protein']
         nameStart = 2
         nameEnd = 14
-        df = pd.read_csv("PandaExpress.csv", sep=',', header=0, names=names)
+        df = pd.read_csv("PandaExpress.csv", sep=',', header=0)
         # print(df.head())
         resValid = True
     elif (res == "Taco Bell"):
-        names = ['Food', 'Type', 'Calories', 'CaloriesFromFat', 'TotalFat', 'SaturatedFat', 'TransFat', 
-            'Cholesterol', 'Sodium', 'TotalCarbohydrates', 'DietaryFiber', 'Sugars', 'Protein']
+        names = ['Food', 'Type', 'Calories', 'Calories From Fat', 'Total Fat', 'Saturated Fat', 'Trans Fat', 
+            'Cholesterol', 'Sodium', 'Total Carbohydrates', 'Dietary Fiber', 'Sugars', 'Protein']
         nameStart = 2
         nameEnd = 13
-        df = pd.read_csv("TacoBell.csv", sep=',', header=0, names=names)
+        df = pd.read_csv("TacoBell.csv", sep=',', header=0)
         resValid = True
     else:
         res = input("Error: Please select a valid restaurant: ")
 
 # Select type
-print("Select one of the following food types, type 'Food' to exclude beverages, or type 'All' to select full menu:")
-print("\t", end='')
-print(df.Type.unique())
+print("\nSelect one of the following food types, type 'Food' to exclude beverages, or type 'All' to select full menu:")
+types = df.Type.unique()
+for i, str in enumerate(types):
+    print("\t{0}".format(str))
 typ = input()
 typValid = False
 while (typValid == False):
@@ -49,11 +50,11 @@ while (typValid == False):
         typ = input("Error: Please select a valid type: ")
         
 # Option to set calorie limit
-cal = input("Would you like to set a calorie limit? Type 'Yes' or 'No': ")
+cal = input("\nWould you like to set a calorie limit?\n\tYes\n\tNo\n")
 calValid = False
 while (calValid == False):
     if (cal == "Yes"):
-        calMax = input("Enter a value between {0} and {1}: ".format(df.Calories.min(), df.Calories.max()))
+        calMax = input("\nEnter a value between {0} and {1}: ".format(df.Calories.min(), df.Calories.max()))
         if calMax.isdigit():
             df = df[df.Calories <= int(calMax)]
             calValid = True
@@ -62,11 +63,13 @@ while (calValid == False):
     elif (cal == "No"):
         calValid = True
     else:
-        cal = input("Error: Please select Yes or No")
+        cal = input("Error: Please select Yes or No: ")
 
 # Select the first criteria
-print("Please select your criteria to filter by: ")
-print(names[nameStart:nameEnd])
+print("\nSelect your first criterion: ")
+nameSubset = names[nameStart:nameEnd]
+for i, str in enumerate(nameSubset):
+    print("\t{0}".format(str))
 crit1 = input()
 # print(crit1)
 crit1Valid = False
@@ -75,8 +78,8 @@ while (crit1Valid == False):
         # print("True\n")
         crit1Valid = True
     else:
-        crit1 = input("Error: Please select a valid criteria: ")
-sort1 = input("Would you like high or low {0}? Type 'High' or 'Low' to select: ".format(crit1))
+        crit1 = input("Error: Please select a valid criterion: ")
+sort1 = input("\nWould you like high or low {0}?\n\tHigh\n\tLow\n".format(crit1))
 sort1Valid = False
 while (sort1Valid == False):
     if (sort1 == "High" or sort1 == "Low"):
@@ -86,18 +89,22 @@ while (sort1Valid == False):
         sort1 = input("Error: Please select 'High' or 'Low': ")
 
 # Filter results
+crit1 = crit1.replace(" ", "")
 df_sub1 = df[crit1]
 q_df = df_sub1.quantile([.25, .75])
 if (sort1 == "High"):
     df = df.query("{0} >= {1}".format(crit1, q_df[0.75]))
+    df = df.sort_values(by=crit1, ascending=False)
     print(df)
 elif (sort1 == "Low"):
     df = df.query("{0} <= {1}".format(crit1, q_df[0.25]))
+    df = df.sort_values(by=crit1, ascending=True)
     print(df)
 
 # Select the second criteria
-print("Please select your criteria to filter by: ")
-print(names[nameStart:nameEnd])
+print("\nSelect your second criterion: ")
+for i, str in enumerate(nameSubset):
+    print("\t{0}".format(str))
 crit2 = input()
 # print(crit1)
 crit2Valid = False
@@ -107,7 +114,7 @@ while (crit2Valid == False):
         crit2Valid = True
     else:
         crit2 = input("Error: Please select a valid criteria: ")
-sort2 = input("Would you like high or low {0}? Type 'High' or 'Low' to select: ".format(crit2))
+sort2 = input("\nWould you like high or low {0}?\n\tHigh\n\tLow\n".format(crit2))
 sort2Valid = False
 while (sort2Valid == False):
     if (sort2 == "High" or sort2 == "Low"):
@@ -117,11 +124,16 @@ while (sort2Valid == False):
         sort2 = input("Error: Please select 'High' or 'Low': ")
 
 # Filter results
+crit2 = crit2.replace(" ", "")
 df_sub2 = df[crit2]
 q_df = df_sub2.quantile([.25, .75])
 if (sort2 == "High"):
     df = df.query("{0} >= {1}".format(crit2, q_df[0.75]))
+    df = df.sort_values(by=crit2, ascending=False)
     print(df)
 elif (sort2 == "Low"):
     df = df.query("{0} <= {1}".format(crit2, q_df[0.25]))
+    df = df.sort_values(by=crit2, ascending=True)
     print(df)
+
+print("\n")
